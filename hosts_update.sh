a1431c86b09eb3d7aa0a39b0df602415418735ba
Wang Zhen <yuchen12@gmail.com>
2013-09-24 18:36:40 +0800

    add /etc/hosts update script
    Signed-off-by: Wang Zhen <yuchen12@gmail.com>

---
+++ hosts_update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+set -o pipefail
+
+bj_url="https://raw.github.com/smarthosts/SmartHosts/master/trunk/hosts"  # beijing
+us_url="https://raw.github.com/smarthosts/SmartHosts/master/trunk/hosts_us" # usa
+
+url=$bj_url
+hosts=/etc/hosts
+tmp_hosts=/tmp/hosts
+filters="GitHub" # "Google\ Services"
+
+sed '/SmartHosts START/,/SmartHosts END/d' $hosts > $tmp_hosts
+
+echo "Downloading hosts from $url"
+wget --quiet $url -O - | dos2unix -q | sed -n '/SmartHosts START/,/SmartHosts END/p' >> $tmp_hosts
+
+for f in $filters; do
+    sed -i '/'"$f"' START/,/'"$f"' END/d' $tmp_hosts
+    echo "Filtered $f"
+done
+
+cp -v $hosts $hosts.bak
+cp -v $tmp_hosts $hosts
+
+rm -rf $tmp_file
