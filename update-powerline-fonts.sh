e7bf5fbd0d0c3c9935112085009e948486a7708c
Wang Zhen <yuchen12@gmail.com>
2014-01-26 16:57:17 +0800

    add powerline fonts
    Signed-off-by: Wang Zhen <yuchen12@gmail.com>

---
+++ update-powerline-fonts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/.fonts ~/.config/fontconfig/conf.d
+curl -L https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf > ~/.fonts/PowerlineSymbols.otf
+curl -L https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf > ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
+fc-cache -vf ~/.fonts
