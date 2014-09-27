a5efa49defeaf6ab2eeff19ad51580dd2d6282ee
Wang Zhen <lazybetrayer@gmail.com>
2014-09-27 21:24:51 +0800

    update
    Signed-off-by: Wang Zhen <lazybetrayer@gmail.com>

--- update-hosts.sh
+++ update-hosts.sh
@@ -3,6 +3,7 @@
 url=https://raw.githubusercontent.com/phoenixlzx/imouto.host/master/imouto.host.txt
 hosts=/etc/hosts
 
-cp $hosts $hosts.bak
-sed -i '/#+BEGIN/,/#+END/d' $hosts
-curl $url | sed -e '1 s/^\xef\xbb\xbf//' -e 's/\r$//g' >> $hosts
+echo "updating hosts files..."
+sudo cp $hosts $hosts.bak
+sudo sed -i '/#+BEGIN/,/#+END/d' $hosts
+curl $url | sed -e '1 s/^\xef\xbb\xbf//' -e 's/\r$//g' | sudo tee -a $hosts >/dev/null
