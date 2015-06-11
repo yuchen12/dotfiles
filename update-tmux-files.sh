90f83200f4e930bf3a55f46cdf3c8e468e6e3465
Wang Zhen <lazybetrayer@gmail.com>
2015-06-11 14:42:45 +0800

    update
    Signed-off-by: Wang Zhen <lazybetrayer@gmail.com>

--- update-tmux-files.sh
+++ update-tmux-files.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-files='bash_completion_tmux.sh dotvim/syntax/tmux.vim'
-url_prefix="http://sourceforge.net/p/tmux/tmux-code/ci/master/tree/examples/"
+files='bash_completion_tmux.sh tmux.vim'
+url_prefix='https://raw.githubusercontent.com/tmux/tmux/master/examples/'
 
 echo "updating tmux files..."
-curl -# -L  "$url_prefix/bash_completion_tmux.sh?format=raw" > ~/.bash_completion_tmux.sh
-curl -# -L "$url_prefix/tmux.vim?format=raw" > ~/.vim/syntax/tmux.vim
+for f in $files; do
+	curl -# -L  "$url_prefix/$f" > ~/".$f"
+done
