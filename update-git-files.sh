b9989a72443500f67907f17232df799105be41b1
Wang Zhen <lazybetrayer@gmail.com>
2015-11-05 15:50:04 +0800

    update
    Signed-off-by: Wang Zhen <lazybetrayer@gmail.com>

--- update-git-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-url_prefix=https://raw.github.com/git/git/master/contrib/completion
-
-echo "updating git files..."
-curl -# -L $url_prefix/git-completion.bash -o ~/.git-completion.bash
-curl -# -L $url_prefix/git-prompt.sh -o ~/.git-prompt.sh
--- update-tmux-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-url_prefix='https://raw.githubusercontent.com/tmux/tmux/master/examples/'
-
-echo "updating tmux files..."
-curl -# -L  "$url_prefix/bash_completion_tmux.sh" -o ~/.bash_completion_tmux.sh
-curl -# -L  "$url_prefix/tmux.vim" -o ~/.vim/syntax/tmux.vim
